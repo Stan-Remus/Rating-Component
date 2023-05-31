a87d3996e0a80e121903477fa3c3a769ac4c8b7b
Robert Popescu <robert.popescu@radcom.ro>
2023-05-31 06:59:09 +0300

    a

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <div className="App">
-      <Routes>
+      <Routes basename="/Rating-Component">
         <Route
           path="/"
           element={<RatingCom setClickedIndex={setClickedIndex} />}
