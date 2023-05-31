a80e643429d4637bfea5cae25ea317c99ee3c1dc
Robert Popescu <robert.popescu@radcom.ro>
2023-05-31 07:14:41 +0300

    c

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    < HashRouter >
-    <App />
-    </HashRouter>
-   
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
